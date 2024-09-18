eb965d0f5abc118b24b0c6664f12e1d7fcc43219
rvillaab <rvabuchaibe@gmail.com>
2024-09-18 21:09:58 +0200

    unused import removed

--- src/main/java/com/rjvilla/quizapp/model/QuizDTO.java
+++ src/main/java/com/rjvilla/quizapp/model/QuizDTO.java
@@ -1,9 +1,7 @@
 package com.rjvilla.quizapp.model;
 
-import com.fasterxml.jackson.annotation.JacksonAnnotation;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Data
 public class QuizDTO {
